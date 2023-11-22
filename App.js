c23a1965aa71f1e220063129b72136a099b1e506
QMMGaneda <142982225+QMMGaneda@users.noreply.github.com>
2023-11-22 22:29:50 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native';
+import MessageList from './components/MessageList';
+import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils';
+import React, { Component } from 'react';
+
+class Messenger extends Component {
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+    ],
+  };
+
+  handlePressMessage = (message) => {
+    console.log('Message pressed', message);
+  };
+
+  renderMessageList() {
+    const { messages } = this.state;
+
+    return (
+      <View style={styles.content}>
+        <MessageList messages={messages} onPressMessage={this.handlePressMessage} />
+      </View>
+    );
+  }
+  render() {
+    return this.renderMessageList();
+  }
+}
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+  },
+});
+
+export default Messenger;
